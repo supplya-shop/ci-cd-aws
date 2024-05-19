3bfb422797d1cddcf4e068967490dc33e596c46a
dvco-xx <osuchukwudavid@gmail.com>
2024-05-19 15:55:58 -0700

    modify auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const passport = require("passport");
 
 const {
-  registerUser,
-  verifyOTPAndGenerateToken,
+  signUp,
+  signUpComplete,
   resendOTP,
   login,
   verifyOTP,
@@ -21,10 +21,9 @@ router.get("/google/redirect", googleAuthCallback);
 
 // Regular Auth routes
 router.post("/login", login);
-// router.post("/vendor/login", vendorLogin);
-router.post("/register", registerUser);
-router.post("/verifyotp", verifyOTP);
-router.post("/verify-otp", verifyOTPAndGenerateToken);
+router.post("/sign-up", signUp);
+router.post("/sign-up-complete", signUpComplete);
+router.post("/verify-otp", verifyOTP);
 router.post("/resend-otp", resendOTP);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
