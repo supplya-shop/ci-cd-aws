d2cb9d1e2916ec2cbd8c398ebcb6518c97d1131e
dvco-xx <osuchukwudavid@gmail.com>
2024-06-14 21:06:05 +0100

    added changePassword route

--- models/User.js
+++ models/User.js
@@ -247,8 +247,8 @@ userSchema.methods.createRefreshToken = function () {
   );
 };
 
-userSchema.methods.comparePassword = async function (loginPassword) {
-  const isMatch = bcrypt.compare(loginPassword, this.password);
+userSchema.methods.comparePassword = async function (password) {
+  const isMatch = await bcrypt.compare(password, this.password);
   console.log("isMatch:", isMatch);
   return isMatch;
 };
